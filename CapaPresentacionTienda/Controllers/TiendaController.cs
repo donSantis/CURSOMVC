72873a9460fc69ee9de54be7443e038b6aaa4e45
Iván Santis <isantis@geovictoria.com>
2023-10-22 12:33:06 -0400

    Update INICIO

--- CapaPresentacionTienda/Controllers/TiendaController.cs
+++ CapaPresentacionTienda/Controllers/TiendaController.cs
@@ -36,25 +36,25 @@ namespace CapaPresentacionTienda.Controllers
 		{
 			List<Producto> lista = new List<Producto>();
 			bool conversion;
-			lista = new CN_Producto().Listar().Select(p => new Producto()
-			{
-				ID_PRODUCTO = p.ID_PRODUCTO,
-				NOMBRE = p.NOMBRE,
-				DESCRIPTION = p.DESCRIPTION,
-				ID_MARCA = p.ID_MARCA,
-				ID_CATEGORIA = p.ID_CATEGORIA,
-				PRECIO = p.PRECIO,
-				STOCK = p.STOCK,
-				RUTA_IMAGEN = p.RUTA_IMAGEN,
-				BASE64 = CN_Recursos.ConvertirBase64(Path.Combine(p.RUTA_IMAGEN, p.NOMBRE_IMAGEN), out conversion),
-				EXTENSION = Path.GetExtension(p.NOMBRE_IMAGEN),
-				ACTIVO = p.ACTIVO,
-			}).Where(p =>
-				p.ID_CATEGORIA.ID_CATEGORIA == (idCategoria == 0 ? p.ID_CATEGORIA.p.ID_CATEGORIA : idCategoria) &&
-				p.ID_MARCA.ID_MARCA == (idMarca == 0 ? p.ID_MARCA.ID_MARCA : idMarca) &&
-				p.STOCK > 0 && p.ACTIVO == true).ToList();
+			//lista = new CN_Producto().Listar().Select(p => new Producto()
+			//{
+			//	ID_PRODUCTO = p.ID_PRODUCTO,
+			//	NOMBRE = p.NOMBRE,
+			//	DESCRIPTION = p.DESCRIPTION,
+			//	ID_MARCA = p.ID_MARCA,
+			//	ID_CATEGORIA = p.ID_CATEGORIA,
+			//	PRECIO = p.PRECIO,
+			//	STOCK = p.STOCK,
+			//	RUTA_IMAGEN = p.RUTA_IMAGEN,
+			//	BASE64 = CN_Recursos.ConvertirBase64(Path.Combine(p.RUTA_IMAGEN, p.NOMBRE_IMAGEN), out conversion),
+			//	EXTENSION = Path.GetExtension(p.NOMBRE_IMAGEN),
+			//	ACTIVO = p.ACTIVO,
+			//}).Where(p =>
+			//	p.ID_CATEGORIA.ID_CATEGORIA == (idCategoria == 0 ? p.ID_CATEGORIA.p.ID_CATEGORIA : idCategoria) &&
+			//	p.ID_MARCA.ID_MARCA == (idMarca == 0 ? p.ID_MARCA.ID_MARCA : idMarca) &&
+			//	p.STOCK > 0 && p.ACTIVO == true).ToList();
 
-				;
+			//	;
 			return Json(new { data = lista }, JsonRequestBehavior.AllowGet);
 		}
 	}
